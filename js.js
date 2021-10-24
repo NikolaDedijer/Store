ab307ab1be6a5b47df27a5aee27aeb31143eec62
Nikola Dedijer <dedijern@gmail.com>
2021-10-24 18:56:24 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -3,39 +3,39 @@ let carts = document.querySelectorAll('.add-cart');
 let products = [{
         name: 'Kolacic',
         tag: 'kolacic',
-        price: 15,
+        price: 15.00,
         inCart: 0
     },
     {
         name: 'Vino',
         tag: 'vino',
-        price: 16,
+        price: 16.00,
         inCart: 0
     },
     {
         name: 'Voce',
         tag: 'voce',
-        price: 17,
+        price: 17.00,
         inCart: 0
     },
     {
         name: 'Ara',
         tag: 'ara',
-        price: 20,
+        price: 20.00,
         inCart: 0
     },
     {
         name: 'Jakna',
         tag: 'jakna',
-        price: 21,
+        price: 21.00,
         inCart: 0
     }
 ]
 
 for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
-        console.log('dodaj');
         cartNumbers(products[i]);
+        totalCost(products[i]);
     })
 }
 
@@ -105,6 +105,7 @@ function displayCart() {
 
     let productContainer = document.querySelector(".products");
     let cartCost = localStorage.getItem("totalCost");
+    console.log(cartCost);
     cartCost = JSON.parse(cartCost);
 
     if (cartItems && productContainer) {
@@ -116,14 +117,14 @@ function displayCart() {
             <img src="./images/${item.tag}.png">
             <span class="name">${item.name}</span>
             
-            <div class="price">$${item.price}</div>
+            <div class="price">$${item.price},00</div>
             <div class="quantity">
             <span class="far fa-arrow-alt-circle-left"></span>
             <span>${item.inCart}</span>
             <span class="far fa-arrow-alt-circle-right"></span>
             </div>
             <div class="total">
-            $${item.inCart * item.price}
+            $${item.inCart * item.price},00
             </div>
             
             `;
@@ -135,13 +136,33 @@ function displayCart() {
         Basket Total
         </h4>
         <h4 class="basketTotal">
-        $${cartCost}
+        $${cartCost},00
         </h4>
         </div>
         `;
+
+
     }
 
 }
 
 onLoadCartNumbers();
 displayCart();
+
+
+function posalji() {
+    let ime = document.getElementById("ime").innerHTML;
+    let tel = document.getElementById("tel").innerHTML;
+    let adresa = document.getElementById("adresa").innerHTML;
+
+    var ajax = new XMLHttpRequest();
+    ajax.open("POST", "server.php", true);
+
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200)
+            console.log(this.responseText);
+    };
+
+    ajax.send("ime" + ime + "&tel" + tel + "&adresa" + adresa);
+    return false;
+}
